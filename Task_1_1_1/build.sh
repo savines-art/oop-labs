61ef18b6e50734b21c54c7a0dbcded3211ad7942
Egor Savin <ko.artist8664@gmail.com>
2026-09-09 01:55:59 +0700

    New build script

--- Task_1_1_1/build.sh
+++ Task_1_1_1/build.sh
@@ -2,15 +2,18 @@
 
 PROGRAMM="lab.task.HeapSort"
 
+BUILD_DIR=build
 SRC_DIR=src/main/java
-OUT_DIR=build/classes
-DOC_DIR=build/javadoc
+OUT_DIR="$BUILD_DIR"/classes
+DOC_DIR="$BUILD_DIR"/javadoc
 
 mkdir -p "$OUT_DIR" "$DOC_DIR"
 
-find "$SRC_DIR" -name "*.java" | xargs javac -d "$OUT_DIR"
+javac -d "$OUT_DIR" "$SRC_DIR"/lab/task/HeapSort.java
 
-javadoc -d "$DOC_DIR" -sourcepath "$SRC_DIR" -subpackages lab.task
+javadoc -d "$DOC_DIR" -sourcepath "$SRC_DIR" lab.task
 
-java -cp "$OUT_DIR" "$PROGRAMM"
+jar cfe "$BUILD_DIR"/HeapSort.jar "$PROGRAMM" -C "$OUT_DIR" .
+
+java -jar "$BUILD_DIR"/HeapSort.jar
 
--- Task_1_1_1/src/main/java/lab/task/HeapSort.java
+++ Task_1_1_1/src/main/java/lab/task/HeapSort.java
@@ -24,15 +24,16 @@ public class HeapSort {
      * @param end where to stop sifting
      */
     private static void siftDown(int[] arr, int index, int end) {
-        int curr = index;
-        while (curr * 2 + 1 < end) {
-            int next1 = curr * 2 + 1;
-            int next2 = next1 + 1 < end ? next1 + 1 : next1;
-            if (arr[curr] > arr[next1] && arr[curr] > arr[next2]) return;
-
-            int smaller = arr[next1] > arr[next2] ? next1 : next2;
-            swap(arr, curr, smaller);
-            curr = smaller;
+        while (index * 2 + 1 < end) {
+            int next = index * 2 + 1;
+            if (next + 1 < end && arr[next] < arr[next + 1]) {
+                next++;
+            }
+            if (arr[index] > arr[next]) {
+                return;
+            }
+            swap(arr, index, next);
+            index = next;
         }
     }
 
@@ -41,13 +42,11 @@ public class HeapSort {
      * @param arr an array
      */
     private static void arrayToHeap(int[] arr) {
-        if (arr.length < 2) {
+        if (arr.length == 1) {
             return;
         }
-        int middle = arr.length / 2;
-        while (middle > -1) {
+        for (int middle = arr.length / 2; middle > -1; middle--) {
             siftDown(arr, middle, arr.length);
-            middle--;
         }
     }
 
