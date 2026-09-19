7d07f36747486e86a22b73baf1a31a0a38aac575
savines-art <savines-art@users.noreply.github.com>
2026-09-19 18:18:40 +0000

    Deploying to gh-pages from @ savines-art/oop-labs@f42ccc04d6f4c06b0c2d6902ec7e32ecc0da246e 🚀

--- Task_1_1_2/package-search-index.js
+++ Task_1_1_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"sys.pro"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"sys.pro"}];updateSearchResults();
